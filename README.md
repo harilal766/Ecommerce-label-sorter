8efc0a322f4a20ecfac022e5bc414751aa55c27e
harilal766 <harilalsunil2@gmail.com>
2025-06-10 14:26:13 +0530

    added basic project structure

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Ecommerce-label-sorter
+#### Video Demo:  <URL HERE>
+#### Description:
 A python library to sort Ecommerce shipping labelse based on product name, variation and quantity
---
+++ project.py
@@ -0,0 +1,18 @@
+def main():
+    pass
+
+def pdf_reader():
+    pass
+
+def label_summary_creator():
+    pass
+
+def pdf_writer(output_directory:str):
+    pass 
+
+
+
+
+
+if __name__ == "__main__":
+    main()
