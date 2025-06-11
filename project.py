248ec8b6bfae7ef9111d428255f7f3fe12601d4e
harilal766 <harilalsunil2@gmail.com>
2025-06-11 12:10:06 +0530

    headers processed

--- project.py
+++ project.py
@@ -11,31 +11,53 @@ def main():
     """
     try:
         with open('creds.json') as json_file:
-            input_dir = json.load(json_file)["input"]
+            json_dict = json.load(json_file)
+            input_dir = json_dict["input"]
+            output_dir = json_dict["output"]
         platform = "Amazon "
         summary = 0
         
-        print(input_dir)
-        """
+        str = {
+            "vertical_strategy": "lines", 
+            "horizontal_strategy": "lines",
+            "explicit_vertical_lines": [],
+            "explicit_horizontal_lines": [],
+            "snap_tolerance": 3,
+            "snap_x_tolerance": 3,
+            "snap_y_tolerance": 3,
+            "join_tolerance": 3,
+            "join_x_tolerance": 3,
+            "join_y_tolerance": 3,
+            "edge_min_length": 3,
+            "min_words_vertical": 3,
+            "min_words_horizontal": 1,
+            "intersection_tolerance": 3,
+            "intersection_x_tolerance": 3,
+            "intersection_y_tolerance": 3,
+            "text_tolerance": 3,
+            "text_x_tolerance": 3,
+            "text_y_tolerance": 3,
+            #"text_*": …, # See below
+        }
+        
         with pdfplumber.open(input_dir) as pdf_file:
             for page in pdf_file.pages:
                 page_text = page.extract_text()
-                page_table = page.extract_table()
+                page_table = page.extract_tables()
                 
                 if platform.strip().lower() == 'amazon':
-                    df = pd.DataFrame(page_table)
-        """
+                    processed_invoice_data = []
+                    if len(page_table) > 1:
+                        processed_headers = [column.replace("\n", " ") for column in page_table[0][0]]
+                        print(processed_headers)
+                        print("+++")
+
                     
-        
-        
     except Exception as e:
-        pass
+        print(e)
     else:
         return summary
         
-
-
-
 
 
 if __name__ == "__main__":
