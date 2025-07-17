a1c90fa4d89adf5e927a5d710325b5dd96268cba
harilal766 <harilalsunil2@gmail.com>
2025-07-17 16:19:11 +0530

    added module for credentials

--- tests/test_amazon.py
+++ tests/test_amazon.py
@@ -1,13 +1,12 @@
 from sorting_alrogithms.amazon import Amazon 
-from test_project import *
-import pdfplumber
+import pdfplumber, json
+
+from credentials import amzn_input_filepath, non_existing_file, out_dir
 
 amzn = Amazon()
 class Test_Amazon:
     
     def test_find_amazon_page_type():
-        qr_page = amzn_input_filepath
-        
         with pdfplumber.open(amzn_input_filepath) as pdf:
             for page in pdf:
                 page = page.extract_text()
--- tests/test_project.py
+++ tests/test_project.py
@@ -1,12 +1,9 @@
 from project import *
 from sorting_alrogithms.sorting_algorithms import *
+from credentials import amzn_input_filepath, non_existing_file, out_dir
 import json
 
-with open("creds.json","r") as json_file:
-    credentials = json.load(json_file)
-    amzn_input_filepath = credentials["amazon_input_filepath"]
-    non_existing_file = amzn_input_filepath.replace(".pdf",".py")
-    out_dir = amzn_input_filepath.replace(".pdf", "")
+
 
 def test_main():
     # the function should throw an error when presented with an invalid file
