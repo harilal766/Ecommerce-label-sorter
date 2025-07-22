d7f755168a5b161ede2fd2ec2ec580ae3054eee1
harilal766 <harilalsunil2@gmail.com>
2025-07-22 10:16:14 +0530

    bug fixing with output pdf name regex

--- manual.py
+++ manual.py
@@ -4,7 +4,7 @@ from label_sorter.core import LabelSorter
 from label_sorter.platforms.ecommerce.amazon import AmazonLabel
 from tests.filepaths import *
 
-label_inst = LabelSorter(pdf_path=r"D:\5.Amazon\Mathew global\INvoice\21.7.25 prepaid 16.pdf")
+label_inst = LabelSorter(pdf_path=amazon_pdf)
 
 
 print(label_inst.create_sorted_pdf_files())
