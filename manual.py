96bf876d5a5319b87c4117a27d99225d8a6e6deb
harilal766 <harilalsunil2@gmail.com>
2025-07-23 10:31:21 +0530

    added input function to get filepath in manual.py

--- manual.py
+++ manual.py
@@ -1,10 +1,12 @@
 
 from label_sorter.core import LabelSorter
+import re
 
 from label_sorter.platforms.ecommerce.amazon import AmazonLabel
-from tests.filepaths import *
 
-label_inst = LabelSorter(pdf_path=amazon_pdf)
+pdf_input = str(input("Enter the pdf filepath : "))
+pdf_input = re.sub(r'\"|\"','',pdf_input)
+label_inst = LabelSorter(pdf_path=pdf_input)
 
 
 print(label_inst.create_sorted_pdf_files())
