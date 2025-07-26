9ec625168bd82e18e83180dc2dfa228125ddc9b7
harilal766 <harilalsunil2@gmail.com>
2025-07-26 21:02:05 +0530

    adding packages and dependencies to setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="Ecommerce_Label_Sorter",
-    version="0.1.0"
+    version="0.1",
+    packages=["label_sorter"],
+    install_requires = [
+        
+    ]
+    
 )
