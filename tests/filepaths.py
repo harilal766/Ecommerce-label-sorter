1a5f178d901e52f2dccc5c6d917d4b0137c39580
harilal766 <harilalsunil2@gmail.com>
2025-07-27 13:18:32 +0530

    this dist is working on local testing

--- tests/filepaths.py
+++ tests/filepaths.py
@@ -4,14 +4,13 @@ from pathlib import Path
 TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 test_and_root_join = os.path.join(TEST_DIR,"..")
 Root = os.path.abspath(test_and_root_join)
-
-
+test_label_dir = os.path.join(Root,"test_labels")
 
 with open("creds.json","r") as json_file:
     credentials = json.load(json_file)
     
-    amazon_pdf = os.path.join(TEST_DIR, credentials["amazon"])
-    shopify_pdf = os.path.join(TEST_DIR, credentials["shopify"])
+    amazon_pdf = os.path.join(test_label_dir, credentials["amazon"])
+    shopify_pdf = os.path.join(test_label_dir, credentials["shopify"])
     
     
 with open("requirements.txt", "r") as req:
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,10 +1,10 @@
 import sys, os, pytest
 
-#sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from filepaths import *
 from label_sorter.core import LabelSorter
 
-"""
+
 class Test_LabelSorter:
     label_inst = LabelSorter(pdf_path=shopify_pdf)
     
@@ -14,14 +14,10 @@ class Test_LabelSorter:
     }
     
     def test_find_platfrom(self):
-        for key,value in self.files.items():
-            inst = LabelSorter(pdf_path=value)
-            assert inst.find_platform() == key
+        for platform,filename in self.files.items():
+            if filename.endswith('.pdf') and os.path.exists(filename):
+                inst = LabelSorter(pdf_path=filename)
+                assert inst.find_platform() == platform
         
     def test_sort_label(self):
         assert len(self.label_inst.create_sorted_summary().keys()) > 0
-        
-        
-    # need more tests for mixed orders and amazon qr page
-
-"""
